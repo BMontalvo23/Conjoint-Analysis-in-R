6c4565236cfa7ae40d5e8caddf4d5ae42b349b44
Bryan Montalvo <bryanmontalvohernandez@gmail.com>
2023-04-14 18:25:41 -0600

    Chnage

---
+++ Conjoint code.R
@@ -0,0 +1,12 @@
+library(conjoint)
+
+data <- read.csv('raw data.csv')
+data<- data[2:8]
+
+colnames(data) <- c('Live_VOD','cust_stickers_props', 'creators_messaging_and_shout_outs', 'special_chat_previledge', 'subscriber_only_chat', 'ad_free_viewing', 'all') 
+
+colnames(data)
+
+print(round(cov(data),5))
+
+head(data)
